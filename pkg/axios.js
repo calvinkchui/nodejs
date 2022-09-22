39b873cc76a94d80e3cbf46d300b56188e01f657
zerohui <96161394+calvinkchui@users.noreply.github.com>
2022-09-22 10:13:44 +0000

    deprecate Axios & sanity

--- pkg/axios.js
+++ pkg/axios.js
@@ -1,57 +1,57 @@
-
+// Freezed - move to sb-node-vanilla
 module.exports = {
-   test: async function() {
+  test: async function() {
     console.log("testAxios()")
- const axios = require('axios');
-     
-  //await yahoo(axios);
-  await queryString01( axios);
-  await queryString02( axios);
-  console.log("testAxios() end")
-   }
+    const axios = require('axios');
+
+    //await yahoo(axios);
+    await queryString01(axios);
+    await queryString02(axios);
+    console.log("testAxios() end")
+  }
 }
 
-async function yahoo( axios) {
-  console.log('get yahoo:'); 
- 
+async function yahoo(axios) {
+  console.log('get yahoo:');
+
   axios.get('http://www.yahoo.com')
-  .then(function (response) {
-    // handle success
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+    .then(function(response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function() {
+      // always executed
+    });
 }
 
 // QueryString
-async function queryString02( axios) {
-  console.log('get queryString02.data:'); 
+async function queryString02(axios) {
+  console.log('get queryString02.data:');
 
   axios.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {  });
+    .then(function(response) {
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+    .finally(function() { });
 }
 
 // QueryString - array
-async function queryString01( axios) {
-  console.log('get queryString01.data:'); 
+async function queryString01(axios) {
+  console.log('get queryString01.data:');
 
   axios.get('https://postman-echo.com/get?foo1=bar1&foo1=bar2')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {  });
+    .then(function(response) {
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+    .finally(function() { });
 }
--- pkg/sanityClient.js
+++ pkg/sanityClient.js
@@ -1,6 +1,7 @@
+// Freeze - move to sb-node-vanilla
 // https://www.npmjs.com/package/@sanity/client
 module.exports = {
-  
+
   // Setup
   setupSandbox: function() {
     const sanityClient = require('@sanity/client')
@@ -11,16 +12,16 @@ module.exports = {
       token: 'skH0YrDpHXXXmX306RuMEo1OP9olOr03w3zewmIu5HRrOecGRCO6nr9wpEvriVtGM5j51IMbAV8xqMr0kkuPt7A7hj8VQzJDs1mFDy9qnIJqu1gesL1OMTg8k4uN5TXu5bjkvYSoxeSJNu84RUmOwQPP9VuvT0LrVT08ZrdquQZfsaOWkhzm', // sanity-auth-token or leave blank for unauthenticated usage
       useCdn: false, // `false` if you want to ensure fresh data
     })
-  
+
     return client;
   },
-  
+
   // Query 
   queryAllSimpleType: function(client) {
     const query = '*[_type == "simpleType"]'
     // {_id, title}
     const params = {}
-  
+
     client.fetch(query, params).then((items) => {
       console.log('simpleType:')
       items.forEach((item) => {
@@ -29,12 +30,12 @@ module.exports = {
       })
     })
   },
-  
+
   query: function(client, query, params) {
     //const query = '*[_type == "simpleType"]'
     // {_id, title}
-   // const params = {}
-  
+    // const params = {}
+
     client.fetch(query, params).then((items) => {
       items.forEach((item) => {
         console.log(`[${item._id}]:`);
@@ -42,53 +43,53 @@ module.exports = {
       })
     })
   },
-  
+
   // Create
   create: function(client, type, data) {
-  
-    const doc = Object.assign({_type: type} , data);
-  
+
+    const doc = Object.assign({ _type: type }, data);
+
     client.create(doc).then((res) => {
       console.log(`[${type}] was created, document ID is ${res._id}`)
     })
   },
-  
+
   // Update
   updateSimpleType: function(client, docId, data) {
-  
-  var desc = "#API - updated @" + Date();
-  client
-    .patch(docId) // Document ID to patch
-    .set( 
-      Object.assign({ description:  desc}, data)
-    ) // Shallow merge  
-    .commit() // Perform the patch and return a promise
-    .then((updatedDoc) => {
-      console.log('Updated document:')
-      console.log(updatedDoc)
-    })
-    .catch((err) => {
-      console.error('Oh no, the update failed: ', err.message)
-    })
-  
+
+    var desc = "#API - updated @" + Date();
+    client
+      .patch(docId) // Document ID to patch
+      .set(
+        Object.assign({ description: desc }, data)
+      ) // Shallow merge  
+      .commit() // Perform the patch and return a promise
+      .then((updatedDoc) => {
+        console.log('Updated document:')
+        console.log(updatedDoc)
+      })
+      .catch((err) => {
+        console.error('Oh no, the update failed: ', err.message)
+      })
+
   },
-  
+
   // ---------------------------------------------------------------
   test: function() {
-     console.log("test - setup:", new Date())
+    console.log("test - setup:", new Date())
     var client = this.setupSandbox();
-    
+
     //this.demo01(client);
-     console.log("test - demoMedia01:")
+    console.log("test - demoMedia01:")
     this.demoMedia01(client);
   },
-  
-  
-  
-    
+
+
+
+
   demo01: function(client) {
     //var client = this.setupSandbox();
-  
+
     // INSERT  
     /*
     var insertData = { title: "Create by API " + new Date().toISOString(), textContent: "#API" };
@@ -103,17 +104,17 @@ module.exports = {
     this.queryAllSimpleType(client);  
   */
     //
-  
+
   },
-  
+
   demoMedia01: function(client) {
     console.log("demoMedia01:")
 
     // SELECT
-    this.query(client, "*[_type == 'media'][0..0]", {}); 
+    this.query(client, "*[_type == 'media'][0..0]", {});
 
-    this.query(client, "*[_type == 'media']{_id, title, image }[0..0]", {}); 
-    
+    this.query(client, "*[_type == 'media']{_id, title, image }[0..0]", {});
+
   }
 
 }
